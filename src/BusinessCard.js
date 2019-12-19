c950b632b2ad9eae094e5aeea71ab68c957714c3
Pedro Vallese <pedro.vallese@clubpetro.com.br>
2019-12-19 08:56:08 -0300

    fixed layout configuration, using styledcomponents

--- src/BusinessCard.js
+++ src/BusinessCard.js
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components"
+
+
+const BusinessImage = styled.div`
+  background-color: yellow;
+  width: 5vw;
+  height: 5vw;
+  background-image:url(${props => props.image});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  -webkit-box-shadow: 5px 5px 3px -5px #000000;
+  box-shadow: 5px 5px 3px -5px #000000; /* remove if you don't care about IE8 */
+`
+
 const BusinessCard = ({place, jobtitle, date}) => {
+  
   return (
     <div className="businesscard">
-        <div className="image">a</div>
+        <BusinessImage image='https://cdn-ofuxico.akamaized.net/img/upload/noticias/2006/08/01/38388_36.jpg'/>
         <div className="information">
             <div className="place">{place}</div>
             <div className="jobtitle"> {jobtitle}</div>
@@ -12,5 +28,6 @@ const BusinessCard = ({place, jobtitle, date}) => {
     
   );
 };
+
 
 export default BusinessCard;
--- src/BusinessHistory.js
+++ src/BusinessHistory.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-import BuisnessCard from './BusinessCard'
+import BusinessCard from './BusinessCard'
 const BusinessHistory = () => {
   return(
     <div className="businesshistory"><span>Business History</span>
-        <BuisnessCard place="Clubpetro" jobtitle="Fullstack Developer" date="2018 - Now" />
-        <BuisnessCard place="Clubpetro" jobtitle="Fullstack Developer" date="2018 - Now" />
-        <BuisnessCard place="Clubpetro" jobtitle="Fullstack Developer" date="2018 - Now" />
-        <BuisnessCard place="Clubpetro" jobtitle="Fullstack Developer" date="2018 - Now" />
+        <BusinessCard place="Clubpetro" jobtitle="Fullstack Developer" date="2018 - Now" />
+        <BusinessCard place="Atlas Soluções em Tecnologia" jobtitle="Fundador / Vice-Presidência" date="2017 - 2018" />
+        <BusinessCard place="FEJEMG" jobtitle="Coordenador de T.I" date="2016 - 2017" />
+        <BusinessCard place="Unifei Projetos" jobtitle="Gerente de T.I" date="2015 - 2017" />
+
     </div>
   
     );
